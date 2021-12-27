0867f2ca1a17393763d5004c1ae79825e53be857
Heitor Schumann <heitorschumann200@gmail.com>
2021-12-27 20:14:43 -0300

    put a listener on carousel, started model info page, started new css

---
+++ assets/js/eventListenerCarousel.js
@@ -0,0 +1,57 @@
+const item0 =  document.querySelector('#item-0');
+const item1 =  document.querySelector('#item-1');
+const item2 =  document.querySelector('#item-2');
+const item3 =  document.querySelector('#item-3');
+const item4 =  document.querySelector('#item-4');
+const item5 =  document.querySelector('#item-5');
+const item6 =  document.querySelector('#item-6');
+const item7 =  document.querySelector('#item-7');
+const item8 =  document.querySelector('#item-8');
+const item9 =  document.querySelector('#item-9');
+const item10 = document.querySelector('#item-10');
+const item11 = document.querySelector('#item-11');
+
+item0.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca-0.html'
+});
+
+item1.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item2.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item3.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item5.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item6.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item7.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item8.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item9.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item10.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
+item11.addEventListener('click', function(){
+    window.location.href = '../../pages/busca/busca.html'
+});
+
