bbf3a7b80ee5b6b31648a118eb16b63e77998d31
Jaiane <dsjaiane.guimaraes@gmail.com>
2022-01-03 01:55:35 -0300

    validando os inputs(email, senha)

--- pages/cadastro/script.js
+++ pages/cadastro/script.js
@@ -1,3 +1,5 @@
+//requisição e autocomplete 
+
 $("#cepBtn").on("click", function(){
     var cep = $("#cep").val()
     const url = `https://viacep.com.br/ws/${cep}/json/`
@@ -14,3 +16,40 @@ $("#cepBtn").on("click", function(){
         }
     })
 })
+
+
+// verificação de dados  
+
+
+function validaEmail(email){
+   const crc = /\S+@\S+\.\S+/
+   crc.test(email)
+  
+    if(crc.test(email) === true){
+        $("#emailUser").attr('class', 'form-control is-valid')
+    } else {
+        $("#emailUser").attr('class', 'form-control is-invalid')
+    }
+} 
+function validaSenha(senha){
+    //caracteres maiúsculos e minúsculos, caracteres especiais, números e 8 a 20 caracteres
+    const crcS = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/
+    crcS.test(senha)
+
+    if(crcS.test(senha) === true){
+        $("#senhaUser").attr('class', 'form-control is-valid')
+    } else {
+        $("#senhaUser").attr('class', 'form-control is-invalid')
+    }
+    
+
+}
+function validaSenhaCheck(senhaC){
+    if($('#senhaUser').val()== $('#senhaCheck').val()){
+        $("#senhaCheck").attr('class', 'form-control is-valid')
+    }
+    else {
+        $("#senhaCheck").attr('class', 'form-control is-invalid')
+    }
+}
+
