8bb6f94f75ac7f3b409850011f48f092ab0b02c8
Jaiane <dsjaiane.guimaraes@gmail.com>
2022-01-04 15:25:05 -0300

    Arrumando o footer e criação de json/localStorage dos dados dos Usuarios

--- pages/cadastro/script.js
+++ pages/cadastro/script.js
@@ -1,27 +1,26 @@
-var nomeUser = $('#nomeUser').val()
-var sobrenomeUser = $('#sobrenomeUser').val()
-var emailUser = $('#emailUser').val()
-var senhaUser = $('#senhaUser').val()
-
+let nomeUser
+let sobrenomeUser
+let emailUser
+let senhaUser
 
 //requisição e autocomplete
 
-$("#cepBtn").on("click", function(){
-    var cep = $("#cep").val()
+$("#cepBtn").on("click", function () {
+    const cep = $("#cep").val()
     const url = `https://viacep.com.br/ws/${cep}/json/`
-    
+
     $.ajax({
         url: url,
         type: 'GET',
-        success: function (dados){
+        success: function (dados) {
             console.log(dados)
             $('#uf').val(dados.uf)
             $('#cidade').val(dados.localidade)
             $('#bairro').val(dados.bairro)
-            $('#rua').val(dados.logradouro)   
-                      
+            $('#rua').val(dados.logradouro)
+
         },
-        error: function(){
+        error: function () {
             $("#cep").attr('class', 'form-control is-invalid')
         }
     })
@@ -31,35 +30,54 @@ $("#cepBtn").on("click", function(){
 // verificação de dados  
 
 
-function validaEmail(email){
-   const crc = /\S+@\S+\.\S+/
-   crc.test(email)
-  
-    if(crc.test(email) === true){
+function validaEmail(email) {
+    const crc = /\S+@\S+\.\S+/
+    crc.test(email)
+
+    if (crc.test(email) === true) {
         $("#emailUser").attr('class', 'form-control is-valid')
     } else {
         $("#emailUser").attr('class', 'form-control is-invalid')
     }
-} 
-function validaSenha(senha){
+}
+
+function validaSenha(senha) {
     //caracteres maiúsculos e minúsculos, caracteres especiais, números e 8 a 20 caracteres
     const crcS = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/
     crcS.test(senha)
 
-    if(crcS.test(senha) === true){
+    if (crcS.test(senha) === true) {
         $("#senhaUser").attr('class', 'form-control is-valid')
     } else {
         $("#senhaUser").attr('class', 'form-control is-invalid')
     }
-    
+
 
 }
-function validaSenhaCheck(senhaC){
-    if($('#senhaUser').val()== $('#senhaCheck').val()){
+
+function validaSenhaCheck(senhaC) {
+    if ($('#senhaUser').val() == $('#senhaCheck').val()) {
         $("#senhaCheck").attr('class', 'form-control is-valid')
-    }
-    else {
+    } else {
         $("#senhaCheck").attr('class', 'form-control is-invalid')
     }
+}
+
+///armazenamento de dados 
+function registrarDados() {
+    nomeUser = $('#nomeUser').val()
+    sobrenomeUser = $('#sobrenomeUser').val()
+    emailUser = $('#emailUser').val()
+    senhaUser = $('#senhaUser').val()
+    const dadosUsuario = {
+        'nome': nomeUser,
+        'sobrenome': sobrenomeUser,
+        'email': emailUser,
+        'senha': senhaUser,
+        'cep': cep
+    }
+    const stringDados = JSON.stringify(dadosUsuario)
+    localStorage.setItem('dadosString', stringDados)
+    console.log(localStorage.getItem('dadosString'))
 }
 
